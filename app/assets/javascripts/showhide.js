3095b79d5115602f7a9a0bc4639cd28a6ec4f7a7
mholm <mholm@tadl.org>
2018-03-06 18:47:22 -0500

    Things are looking better

--- app/assets/javascripts/showhide.js
+++ app/assets/javascripts/showhide.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-  var button = $('.button1');
-  var container = $('.tohide');
-  container.toggleClass('-tohide').toggle();
-  
-  button.click(function() {
-    container.toggleClass('-tohide').toggle(1000);
-  });
-});
+$(document).ready(function() {
+      var button = $('.button1');
+      var button2 = $('.button2')
+      var container = $('.tohide');
+      container.toggleClass('-tohide').toggle();
+
+      button.click(function() {
+          container.toggleClass('-tohide').toggle(1000);
+        });
+        button2.click(function() {
+          container.toggleClass('-tohide').toggle(1000);
+        });
+      });
